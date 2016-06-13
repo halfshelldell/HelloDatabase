f01035ae6a481a5e8d8a163f69ca376c8d4deb36
halfshelldell <simmonslydell@gmai.com>
2016-06-13 11:40:40 -0400

    initial start

---
+++ src/com/ironyard/Main.java
@@ -0,0 +1,42 @@
+package com.ironyard;
+
+import org.h2.tools.Server;
+
+import java.sql.*;
+
+public class Main {
+
+    public static void main(String[] args) throws SQLException {
+        Server.createWebServer().start();
+
+        Connection conn = DriverManager.getConnection("jdbc:h2:./main");
+
+        Statement stmt = conn.createStatement();
+        stmt.execute("CREATE TABLE IF NOT EXISTS players (id IDENTITY, name VARCHAR, is_alive BOOLEAN, score INT, health DOUBLE)");
+        stmt.execute("INSERT INTO players VALUES (NULL, 'Alice', TRUE, 0, 100)");
+        stmt.execute("UPDATE players SET is_alive = FALSE WHERE name = 'Alice'");
+        stmt.execute("DELETE FROM players WHERE name = 'Alice'");
+
+        // BAD!!!!
+        //String name = "', TRUE, 0, 0); DROP TABLE players; --";
+        //stmt.execute(String.format("INSERT INTO players VALUES (NULL, '%s', TRUE, 0, 100)", nameExample));
+
+        // GOOD (PREPARED STATEMENTS)
+        String name = "Charlie";
+        PreparedStatement stmt2 = conn.prepareStatement("INSERT INTO players VALUES (NULL, ?, ?, 0, 100)");
+        stmt2.setString(1, name);
+        stmt2.setBoolean(2, true);
+        stmt2.execute();
+
+        PreparedStatement stmt3 = conn.prepareStatement("SELECT * FROM players");
+        ResultSet results = stmt3.executeQuery();
+        while (results.next()) {
+            int id = results.getInt("id");
+            String name1 = results.getString("name");
+            boolean isAlive = results.getBoolean("is_alive");
+            int scoreTotal = results.getInt("score");
+            double health = results.getDouble("health");
+            System.out.println(id + " " + name1 + " " + isAlive + " " + scoreTotal + " " + health);
+        }
+    }
+}
